ed18fa9d48c96c8cab6880175da9478673e9fe4f
JaeWookYoon <51107995+jaewook1048@users.noreply.github.com>
2023-03-17 22:15:36 +0900

    2023-03-17 node 3주차
    2023-03-17 node 3주차

---
+++ week3/2.1.4-1.js
@@ -0,0 +1,23 @@
+function add1(x, y) {
+  return x + y;
+}
+
+const add2 = (x, y) => {
+  return x + y;
+};
+
+const add3 = (x, y) => x + y;
+
+const add4 = (x, y) => (x + y);
+
+function not1(x) {
+  return !x;
+}
+
+const not2 = x => !x;
+
+//console.log(not2(0));
+
+const aa = (x) => console.log(x);
+aa("아앙");
+
